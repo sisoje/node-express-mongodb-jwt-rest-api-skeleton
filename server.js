1f46c091ee95da4e0567ff8a14a4e225caa3c525
Lazar Otasevic <redhotbits@gmail.com>
2019-09-28 11:40:07 +0200

    try to fix

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(require('./app/routes'))
 // Init MongoDB
 initMongo()
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log('Express server listening on port', port)
 })
 
